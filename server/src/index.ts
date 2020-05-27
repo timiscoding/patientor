22d79ff28de0098b3deb7047360419c950eff1dc
Tim <timiscoding@gmail.com>
2020-05-27 13:57:08 +1000

    Add gitignore and update port

--- server/src/index.ts
+++ server/src/index.ts
@@ -22,7 +22,7 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 
 app.get("/api/ping", (_req, res) => {
   res.send("pong");
